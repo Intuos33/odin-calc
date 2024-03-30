892cb811cc4aba2c8a333b753f705d81eb02506e
Intuos33 <bmiller.development@gmail.com>
2024-03-30 17:14:04 -0400

    js start

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var numOne;
+var numTwo;
+var operator;
+
+function add(numOne, numTwo){
+    return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo){
+    return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo){
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo){
+    return numOne / numTwo;
+}
+
+function operate(numOne, operator, numTwo){
+
+}
+
+const numBtn = document.querySelector('#num-btb');
+numBtn.addEventListener('click', () => {
+    
+});
