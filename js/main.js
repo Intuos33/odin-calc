b35113b6ccb1dc0cbc9b0c5012e1c13e86f26d17
Intuos33 <bmiller.development@gmail.com>
2024-04-01 19:30:34 -0400

    completed the basic functionality of the calulator with proper display functions

--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
-var numOne;
-var numTwo;
-var operator;
 
-function add(numOne, numTwo){
-    return numOne + numTwo;
+let buttons = document.querySelectorAll('button');
+let display = document.querySelector('.display');
+let numOne = '';
+let numTwo = '';
+let numHolder = '';
+let operator = '';
+
+buttons.forEach(button => button.addEventListener('click', handleButtonClick));
+
+function calculate(numOne, numTwo, operator){
+    if(operator == '+'){
+        display.textContent = Number(numOne) + Number(numTwo);
+    }
+    else if(operator == '-'){
+        display.textContent = Number(numOne) - Number(numTwo);
+    }
+    else if(operator == '*'){
+        display.textContent = Number(numOne) * Number(numTwo);
+    }
+    else if(operator == '/'){
+        display.textContent = Number(numOne) / Number(numTwo);
+    }
 }
 
-function subtract(numOne, numTwo){
-    return numOne - numTwo;
+function handleButtonClick(e){
+    let btnTarget = e.target;
+    let btnValue = btnTarget.textContent;
+
+    if(btnTarget.id == 'num-btn'){
+        display.textContent += btnValue;
+    } 
+    if(btnTarget.id == 'op-btn'){
+        numOne = display.textContent;
+        operator = btnValue;
+        display.textContent = '';
+    } 
+    if(btnTarget.id == 'eq-btn'){
+        numTwo = display.textContent;
+        calculate(numOne, numTwo, operator);
+    }
+    if(btnTarget.id == 'cls-btn'){
+        display.textContent = '';
+        numOne = '';
+        numTwo = '';
+        operator = '';
+    }
 }
-
-function multiply(numOne, numTwo){
-    return numOne * numTwo;
-}
-
-function divide(numOne, numTwo){
-    return numOne / numTwo;
-}
-
-function operate(numOne, operator, numTwo){
-
-}
-
-const numBtn = document.querySelector('#num-btb');
-numBtn.addEventListener('click', () => {
-    
-});
